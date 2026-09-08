c56bd547258e82dd82178db266367aefe638639d
Reivo <reivo.meedla@voco.ee>
2026-09-08 11:59:58 +0000

    3. Komponendi sisustamine andmetega

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import './components/ExpenseItem.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const data = [{
+    date: new Date(2024, 10, 12),
+    title: 'New book',
+    price: 30.99
+
+  },
+  {
+    date: new Date(2020, 4, 20),
+    title: 'Doohickey',
+    price: 99.99
+  }]
+
   return (
     <div className="App">
-        <ExpenseItem />
-        <ExpenseItem />
+      <ExpenseItem data={data[0]}/>
+      <ExpenseItem data={data[1]}/>
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+    
+    console.log(props)
+
     return (
-        <div className='expense-item'>
-            <div>Date</div>
-            <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Price</div>
+        <div>
+            <div className='expense-item'>
+                <div>{props.data.date.toString()}</div>
+                <div className='expense-item__description'>
+                    <h2>{props.data.title}</h2>
+                    <div className='expense-item__price'>{props.data.price}</div>
+                </div>
             </div>
         </div>
     )
